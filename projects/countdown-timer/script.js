c2c422e54e02d6284cb4f4b4dc6beae863e179b9
saisravan84 <saisravan84@gmail.com>
2020-09-14 00:50:50 +0530

    first project

---
+++ projects/countdown-timer/script.js
@@ -0,0 +1,25 @@
+let headerMain = document.querySelector("#header-main");
+let dayCount = document.querySelector("#day-count");
+let hourCount = document.querySelector("#hour-count");
+let minCount = document.querySelector("#min-count");
+let secCount = document.querySelector("#sec-count");  
+const countDownDate = "9 Oct 2020";
+const headingData = "Sravan's Birthday";
+
+const formatCount = (count) => `${`${count}`.length < 2 ? 0 : ''}${count}`;
+
+const execute = () => {
+    let todayDate = new Date();
+    let diffDate = new Date(countDownDate) - todayDate;
+    let seconds = Math.floor(diffDate/1000);
+    let minutes = Math.floor(seconds/60);
+    let hours = Math.floor(minutes/60);
+    let days = Math.floor(hours/24);
+    secCount.textContent = formatCount(seconds % 60);
+    minCount.textContent = formatCount(minutes % 60);
+    hourCount.textContent = formatCount(hours % 24);
+    dayCount.textContent = formatCount(days);
+}
+headerMain.textContent = headingData;
+execute();
+setInterval(execute, 1000);
